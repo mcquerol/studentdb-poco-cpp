c7cde2212fd93e8a7996999f87d91b02582537c6
stmacebal <mateo.cebquerol@gmail.com>
2022-05-20 09:13:06 +0200

    Adding myCode\CEnrollment.h on 2022-05-20

---
+++ myCode/CEnrollment.h
@@ -0,0 +1,25 @@
+#ifndef CENROLLMENT_H
+#define CENROLLMENT_H
+
+#include "CCourse.h"
+#include <string>
+
+class CEnrollment {
+private:
+    float grade;
+    std::string semester;
+    /**
+     * @link aggregation
+     */
+   const CCourse* course;
+public:
+    CEnrollment(std::string semester, CCourse* course);
+
+    float getGrade() const;
+    std::string getSemester()const ;
+    const CCourse* getCourse() const;
+
+	void setGrade(float grade);
+};
+
+#endif /* CENROLLMENT_H */
