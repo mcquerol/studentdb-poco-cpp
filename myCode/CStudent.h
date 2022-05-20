913af91b0429ffcbada7738842bccc609a2c8c40
stmacebal <mateo.cebquerol@gmail.com>
2022-05-20 09:13:07 +0200

    Adding myCode\CStudent.h on 2022-05-20

---
+++ myCode/CStudent.h
@@ -0,0 +1,41 @@
+#ifndef CSTUDENT_H
+#define CSTUDENT_H
+
+#include <string>
+#include <vector>
+#include <Poco/Data/Date.h>
+
+#include "CAddress.h"
+#include "CEnrollment.h"
+
+class CStudent {
+private:
+    static unsigned int nextMatrikelNumber;
+    unsigned int maktrikelNumber;
+    std::string firstName;
+    std::string lastName;
+    Poco::Data::Date dateOfBirth;
+    /**
+     * @link aggregationByValue
+     */
+    std::vector<CEnrollment> enrollments;
+    /**
+     * @link aggregationByValue
+     */
+
+public:
+    CStudent(std::string firstName, std::string lastName, Poco::Data::Date dateOfBirth, std::string street, unsigned short postalCode, std::string cityName, std::string additionalInfo);
+
+    CAddress address;
+
+    const unsigned int getMatrikelNumber() const;
+	const std::string& getFirstName() const;
+	const std::string& getLastName() const;
+	const Poco::Data::Date& getDateOfBirth() const;
+
+	void setFirstName(const std::string &firstName);
+	void setLastName(const std::string &lastName);
+	void setDateOfBirth(const Poco::Data::Date &dateOfBirth);
+
+};
+#endif /* CSTUDENT_H */
