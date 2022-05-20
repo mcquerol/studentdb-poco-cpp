89207b9753ef1ce0cd6bcf54d0fe938f56586280
stmacebal <mateo.cebquerol@gmail.com>
2022-05-20 09:13:08 +0200

    Adding myCode\CStudentDb.h on 2022-05-20

---
+++ myCode/CStudentDb.h
@@ -0,0 +1,38 @@
+#ifndef CSTUDENTDB_H
+#define CSTUDENTDB_H
+
+#include <map>
+#include <memory>
+
+#include "CStudent.h"
+#include "CCourse.h"
+
+class CStudentDb {
+
+private:
+	/*
+	 * both of these maps represent the students and courses
+	 */
+    std::map<int,CStudent> students;
+    std::map<int,std::unique_ptr<const CCourse>> courses;
+
+public:
+
+    /*
+     * empty constructor
+     */
+    CStudentDb();
+
+    /*
+     * map setters and getters
+     * they return the entire maps as well as allow to add values to the maps
+     * by pointer and reference respectively
+     */
+	const std::map<int, std::unique_ptr<const CCourse> >& getCourses() const;
+	const std::map<int, CStudent>& getStudents() const;
+
+	void setCourse(CCourse *course);
+	void setStudent(CStudent &student);
+
+};
+#endif /* CSTUDENTDB_H */
