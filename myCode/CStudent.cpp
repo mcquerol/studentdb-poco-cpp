404935ea7b0270539c36d2c1ff1ac794030a77bf
stmacebal <mateo.cebquerol@gmail.com>
2022-05-20 09:13:07 +0200

    Adding myCode\CStudent.cpp on 2022-05-20

---
+++ myCode/CStudent.cpp
@@ -0,0 +1,29 @@
+#include "CStudent.h"
+
+using namespace std;
+
+unsigned int CStudent::nextMatrikelNumber = 100000;
+
+CStudent::CStudent(std::string firstName, std::string lastName, Poco::Data::Date dateOfBirth, std::string street, unsigned short postalCode, std::string cityName, std::string additionalInfo)
+: address{street, postalCode, cityName, additionalInfo}
+{
+	this->firstName = firstName;
+	this->lastName = lastName;
+	this->dateOfBirth = dateOfBirth;
+	this->maktrikelNumber = CStudent::nextMatrikelNumber++;
+}
+
+const unsigned int CStudent::getMatrikelNumber() const
+{
+	return this->maktrikelNumber;
+}
+
+const std::string& CStudent::getFirstName() const
+{
+	return this->firstName;
+}
+
+const std::string& CStudent::getLastName() const
+{
+	return this->lastName;
+}
