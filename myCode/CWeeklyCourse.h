03b316fad84813d71acf52e394ee469bdd02fcb3
stmacebal <mateo.cebquerol@gmail.com>
2022-05-20 09:13:08 +0200

    Adding myCode\CWeeklyCourse.h on 2022-05-20

---
+++ myCode/CWeeklyCourse.h
@@ -0,0 +1,30 @@
+#ifndef CWEEKLYCOURSE_H
+#define CWEEKLYCOURSE_H
+
+#include "CCourse.h"
+
+#include <Poco/DateTime.h>
+#include <Poco/Data/Time.h>
+
+class CWeeklyCourse : public CCourse {
+private:
+
+    Poco::DateTime::DaysOfWeek dayOfWeek;
+    Poco::Data::Time startTime;
+    Poco::Data::Time endTime;
+
+public:
+
+    CWeeklyCourse(unsigned int courseKey, std::string title, unsigned char major, float creditPoints);
+    ~CWeeklyCourse();
+
+    Poco::DateTime::DaysOfWeek getDayOfWeek() const;
+	const Poco::Data::Time& getStartTime() const;
+	const Poco::Data::Time& getEndTime() const;
+
+	void setDayOfWeek(Poco::DateTime::DaysOfWeek dayOfWeek);
+	void setStartTime(const Poco::Data::Time &startTime);
+	void setEndTime(const Poco::Data::Time &endTime);
+
+};
+#endif /* CWEEKLYCOURSE_H */
