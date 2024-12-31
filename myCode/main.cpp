fbda1cdeae0aa0d7ac7b890b1d865dc9f7d164ea
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-12-31 18:42:02 +0100

    testing that the poco library is working

--- myCode/main.cpp
+++ myCode/main.cpp
@@ -1,11 +1,14 @@
-#include "CSimpleUI.h"
+#include <Poco/LocalDateTime.h>
+#include <Poco/DateTimeFormatter.h>
+#include <iostream>
 
-int main (void)
-{
-	CStudentDb db;
-	CSimpleUI ui(db);
+int main() {
+    // Get the current local date and time
+    Poco::LocalDateTime localNow;
 
-	ui.run();
+    // Format the current local date and time as a string
+    std::string formattedLocalDateTime = Poco::DateTimeFormatter::format(localNow, "%Y-%m-%d %H:%M:%S");
+    std::cout << "Local DateTime: " << formattedLocalDateTime << std::endl;
 
-	return 0;
+    return 0;
 }
