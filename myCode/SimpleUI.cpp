445988618153226de8daf46ca6ef8811f30831ea
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-01-05 13:12:25 +0100

    implemented switch statement with input and helper functions

--- myCode/SimpleUI.cpp
+++ myCode/SimpleUI.cpp
@@ -1,10 +1,82 @@
 #include "SimpleUI.h"
+#include <iostream>
 
+using namespace std;
 
 SimpleUI::SimpleUI(StudentDb &db)
 {
 }
 
 void SimpleUI::run()
+{
+
+	int choice;
+
+	cout << "Select an option:" << endl;
+	cout << "1. Add new course" << endl;
+	cout << "2. List courses" << endl;
+	cout << "3. Add new student" << endl;
+	cout << "4. Add enrollment" << endl;
+	cout << "5. Print student" << endl;
+	cout << "6. Search student" << endl;
+	cout << "7. Update student" << endl;
+	cout << "or type 0 to terminate" << endl;
+
+	cin << choice;
+
+	switch(choice)
+	{
+		case 1 :
+			addNewCourse();
+			break;
+		case 2 :
+			listCourses();
+			break;
+		case 3 :
+			addNewStudent();
+			break;
+		case 4 :
+			addEnrollment();
+			break;
+		case 5 :
+			printStudent();
+			break;
+		case 6 :
+			searchStudent();
+			break;
+		case 7 :
+			updateStudent();
+			break;
+		case 0 :
+			return;
+	}
+
+}
+
+void SimpleUI::addNewCourse()
+{
+}
+
+void SimpleUI::listCourses()
+{
+}
+
+void SimpleUI::addNewStudent()
+{
+}
+
+void SimpleUI::addEnrollment()
+{
+}
+
+void SimpleUI::printStudent()
+{
+}
+
+void SimpleUI::searchStudent()
+{
+}
+
+void SimpleUI::updateStudent()
 {
 }
