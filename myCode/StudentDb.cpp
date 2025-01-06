b5a9238af688c69115dd67abe94aa76d309a6b70
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-01-06 01:37:07 +0100

    implemented setter methods to add a new course and student to the map

--- myCode/StudentDb.cpp
+++ myCode/StudentDb.cpp
@@ -21,8 +21,12 @@ const std::map<int, Student>& StudentDb::getStudents() const
 
 void StudentDb::setCourse(Course *course)
 {
+	// move a unique pointer into the courses map using the courseid as the key of the par in thte map
+	courses.insert({course->getCourseKey(), std::move(std::unique_ptr<Course>(course))});
 }
 
 void StudentDb::setStudent(Student student)
 {
+	// add an entry to the students map: matrtikelnumber and the student object
+	students.insert({student.getMatrikelNumber(), student});
 }
