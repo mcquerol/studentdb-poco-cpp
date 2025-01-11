6fdf0cbf2b884fa9e0f66ec43a57f0f7926852b0
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-01-11 17:28:16 +0100

    added setters

--- myCode/StudentDb.cpp
+++ myCode/StudentDb.cpp
@@ -1,6 +1,5 @@
 #include "StudentDb.h"
 
-
 StudentDb::StudentDb()
 {
 }
@@ -19,14 +18,14 @@ const std::map<int, Student>& StudentDb::getStudents() const
 	return students;
 }
 
-void StudentDb::setCourse(Course *course)
-{
-	// move a unique pointer into the courses map using the courseid as the key of the par in thte map
-	courses.insert({course->getCourseKey(), std::move(std::unique_ptr<Course>(course))});
-}
+//void StudentDb::setCourse(std::unique_ptr<Course> course)
+//{
+//    // Move the unique pointer into the courses map using the course id as the key
+//    courses.insert({course->getCourseKey(), std::move(course)});
+//}
 
-void StudentDb::setStudent(Student student)
-{
-	// add an entry to the students map: matrtikelnumber and the student object
-	students.insert({student.getMatrikelNumber(), student});
-}
+//void StudentDb::setStudent(Student student)
+//{
+//	// add an entry to the students map: matrtikelnumber and the student object
+//	students.insert({student.getMatrikelNumber(), student});
+//}
