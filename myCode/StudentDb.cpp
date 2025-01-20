79c647cac4ff169c1b576ac33f7b306433186db5
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-01-20 18:19:12 +0100

    changed insert to implace on setters for clarity

--- myCode/StudentDb.cpp
+++ myCode/StudentDb.cpp
@@ -21,11 +21,12 @@ const std::map<int, Student>& StudentDb::getStudents() const
 void StudentDb::setCourse(std::unique_ptr<Course> course)
 {
     // Move the unique pointer into the courses map using the course id as the key
-    courses.insert({course->getCourseKey(), std::move(course)});
+    courses.emplace(course->getCourseKey(), std::move(course));
 }
 
 void StudentDb::setStudent(Student student)
 {
 	// add an entry to the students map: matrtikelnumber and the student object
-	students.insert({student.getMatrikelNumber(), student});
+	students.emplace(student.getMatrikelNumber(), student);
+
 }
