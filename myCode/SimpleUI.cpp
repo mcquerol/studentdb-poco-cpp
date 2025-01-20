88a0b8804c7a56777171dc5e2e5cb6a4018e9e4a
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-01-20 18:18:52 +0100

    implemented add course method

--- myCode/SimpleUI.cpp
+++ myCode/SimpleUI.cpp
@@ -1,4 +1,8 @@
 #include "SimpleUI.h"
+#include "Course.h"
+#include "WeeklyCourse.h"
+#include "BlockCourse.h"
+
 #include <iostream>
 
 using namespace std;
@@ -56,6 +60,61 @@ void SimpleUI::run()
 
 void SimpleUI::addNewCourse()
 {
+	cout << "-------------------------" << endl;
+	cout << "1. Add new course" << endl;
+	cout << "-------------------------" << endl;
+	cout << "Enter W for Weeklycourse or B for Blockcourse" << endl;
+	char courseChoice;
+	cin >> courseChoice;
+
+
+	unsigned int courseKey;
+	string title;
+	string major;
+	float creditPoints;
+	int dayOfWeek;
+	int startHour, startMinute, startSecond;
+	int endHour, endMinute, endSecond;
+
+	cout << "Enter Course key: ";
+	cin >> courseKey;
+	cout << "Enter Module title: ";
+	cin >> title;
+	cout << "Enter Major (char): ";
+	cin >> major;
+	cout << "Enter Credit points: ";
+	cin >> creditPoints;
+	cout << "Enter day of the week (0-6): ";
+	cin >> dayOfWeek;
+	cout << "Enter start time (h m s): ";
+	cin >> startHour >> startMinute >> startSecond;
+	cout << "Enter end time (h m s): ";
+	cin >> endHour >> endMinute >> endSecond;
+
+	Poco::Data::Time startTime = {startHour, startMinute, startSecond};
+	Poco::Data::Time endTime = {endHour, endMinute, endSecond};
+
+	Poco::Data::Date startDate = {startYear, startMonth, startDay};
+	Poco::Data::Date endDate = {endYear, endMonth, endDay};
+
+
+	if(courseChoice == 'B')
+	{
+		std::unique_ptr<BlockCourse> blockCourse = std::make_unique<BlockCourse>(courseKey, "", major, creditPoints);
+		blockCourse->setStartTime(startTime);
+		blockCourse->setEndTime({endHour, endMinute, endSecond});
+		blockCourse->setStartDate({startDate})
+		db->setCourse(std::move(blockCourse));
+	}
+	else if(courseChoice == 'W')
+	{
+		std::unique_ptr<WeeklyCourse> weeklyCourse = std::make_unique<WeeklyCourse>(courseKey, "", major, creditPoints);
+		weeklyCourse->setDayOfWeek((Poco::DateTime::DaysOfWeek)dayOfWeek);
+		weeklyCourse->setStartTime({startHour, startMinute, startSecond});
+		weeklyCourse->setEndTime({endHour, endMinute, endSecond});
+		db->setCourse(std::move(weeklyCourse));
+	}
+
 }
 
 void SimpleUI::listCourses()
