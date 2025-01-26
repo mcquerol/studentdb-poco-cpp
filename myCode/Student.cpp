ea4149517ea9ce050e1368b6ab5dd5156491f4b2
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-01-26 07:11:55 +0100

    added setters

--- myCode/Student.cpp
+++ myCode/Student.cpp
@@ -16,11 +16,30 @@ Student::~Student()
 {
 }
 
+void Student::setAddress(const Address &address)
+{
+	this->address = address;
+}
+
+void Student::setDateOfBirth(const Poco::Data::Date &dateOfBirth)
+{
+	this->dateOfBirth = dateOfBirth;
+}
+
+void Student::setFirstName(const std::string &firstName)
+{
+	this->firstName = firstName;
+}
+
+void Student::setLastName(const std::string &lastName)
+{
+	this->lastName = lastName;
+}
+
 const Poco::Data::Date& Student::getDateOfBirth() const
 {
 	return dateOfBirth;
 }
-
 
 const std::string& Student::getFirstName() const
 {
@@ -42,7 +61,17 @@ const std::vector<Enrollment>& Student::getEnrollments() const
 	return enrollments;
 }
 
+std::vector<Enrollment>& Student::getEnrollments()
+{
+	return enrollments;
+}
+
+
 void Student::setEnrollment(float grade, std::string semester, Course *course)
 {
 	enrollments.emplace_back(grade, semester, course);
+}
+
+void Student::removeEnrollment(size_t index) {
+    enrollments.erase(enrollments.begin() + index);
 }
