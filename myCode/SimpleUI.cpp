064b041c7b79feed49fd40ecfbfb6b8aa87c1c98
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-01-28 21:06:34 +0100

    fixed run method by adding the switch in an infinite loop

--- myCode/SimpleUI.cpp
+++ myCode/SimpleUI.cpp
@@ -15,48 +15,50 @@ SimpleUI::SimpleUI(StudentDb &db)
 
 void SimpleUI::run()
 {
-
 	int choice;
 
-	cout << "Select an option:" << endl;
-	cout << "1. Add new course" << endl;
-	cout << "2. List courses" << endl;
-	cout << "3. Add new student" << endl;
-	cout << "4. Add enrollment" << endl;
-	cout << "5. Print student" << endl;
-	cout << "6. Search student" << endl;
-	cout << "7. Update student" << endl;
-	cout << "or type 0 to terminate" << endl;
-
-	cin >> choice;
-
-	switch(choice)
+	while(1)
 	{
-		case 1 :
-			addNewCourse();
-			break;
-		case 2 :
-			listCourses();
-			break;
-		case 3 :
-			addNewStudent();
-			break;
-		case 4 :
-			addEnrollment();
-			break;
-		case 5 :
-			printStudent();
-			break;
-		case 6 :
-			searchStudent();
-			break;
-		case 7 :
-			updateStudent();
-			break;
-		case 0 :
-			return;
-	}
+		cout << "Select an option:" << endl;
+		cout << "1. Add new course" << endl;
+		cout << "2. List courses" << endl;
+		cout << "3. Add new student" << endl;
+		cout << "4. Add enrollment" << endl;
+		cout << "5. Print student" << endl;
+		cout << "6. Search student" << endl;
+		cout << "7. Update student" << endl;
+		cout << "or type 0 to terminate" << endl;
 
+		cin >> choice;
+
+		switch(choice)
+		{
+			case 1 :
+				addNewCourse();
+				break;
+			case 2 :
+				listCourses();
+				break;
+			case 3 :
+				addNewStudent();
+				break;
+			case 4 :
+				addEnrollment();
+				break;
+			case 5 :
+				printStudent();
+				break;
+			case 6 :
+				searchStudent();
+				break;
+			case 7 :
+				updateStudent();
+				break;
+			case 0 :
+				cerr << "Program Terminated" << endl;
+				return;
+		}
+	}
 }
 
 void SimpleUI::addNewCourse()
@@ -88,7 +90,7 @@ void SimpleUI::addNewCourse()
 	cin >> courseKey;
 	cout << "Enter Module title: ";
 	cin >> title;
-	cout << "Enter Major (char): ";
+	cout << "Enter Major: ";
 	cin >> major;
 	cout << "Enter Credit points: ";
 	cin >> creditPoints;
