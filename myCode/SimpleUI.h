c61ddf1c0ffd7744c6f0344b469f1c12bf97910b
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-01-29 04:07:09 +0100

    added function declarations of validation functions

--- myCode/SimpleUI.h
+++ myCode/SimpleUI.h
@@ -15,6 +15,16 @@ public:
 	SimpleUI(StudentDb& db);
 	void run();
 
+	/* Error handling and input validation help functions */
+	Poco::Data::Date getValidatedDate(const std::string& prompt);
+	Poco::Data::Time getValidatedTime(const std::string& prompt);
+	Poco::DateTime::DaysOfWeek getValidatedDayOfWeek(const std::string& prompt);
+	unsigned int getValidatedUnsignedInt(const std::string& prompt, unsigned int min = 0, unsigned int max = UINT_MAX);
+	float getValidatedFloat(const std::string& prompt, float min = 1.0, float max = 5.0);
+	std::string getValidatedString(const std::string& prompt, bool allowNumbers = true);
+	std::string getValidatedMajor(const std::string& prompt);
+	unsigned short getValidatedPostalCode(const std::string& prompt);
+
 	/* Helper functions */
 	void addNewCourse();
 	void listCourses();
