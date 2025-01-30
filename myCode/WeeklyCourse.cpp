fa845f114724f497ed4aca018f0cdf7f356bf86d
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-01-30 05:57:40 +0100

    corrected ostream order

--- myCode/WeeklyCourse.cpp
+++ myCode/WeeklyCourse.cpp
@@ -47,6 +47,7 @@ void WeeklyCourse::setStartTime(const Poco::Data::Time &startTime)
 
 void WeeklyCourse::write(std::ostream &out) const
 {
+	out << 'W;';
 	write(out);
-	out << 'W;' << dayOfWeek << ";" << startTime << ";" << endTime << "\n";
+	out << dayOfWeek << ";" << startTime << ";" << endTime << "\n";
 }
