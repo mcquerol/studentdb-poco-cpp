d4384b466e91cfcc4461d34f9c59cbf7c42fbb9a
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-03-31 23:36:34 +0200

    removed a getline so it did not parse extra data

--- myCode/Enrollment.cpp
+++ myCode/Enrollment.cpp
@@ -45,23 +45,13 @@ void Enrollment::write(std::ostream &out) const
 
 void Enrollment::read(std::istream &in)
 {
-//	string line;
-//	getline(in, semester, ';');
-//	getline(in, line, ';');
-//	grade = (stof(line));  // Convert string to float
+    std::string temp;
 
-    std::string temp, line;
+    // Read semester (string)
+    getline(in, semester, ';');
+    // Read grade (float)
+    getline(in, temp, '\n');
 
-    std::cout << "Debug: Reading Enrollment line..." << std::endl;
-    getline(in, line);
-    std::cout << "Debug: Read line: '" << line << "'" << std::endl;
-
-    std::istringstream iss(line);
-
-    getline(iss, semester, ';');
-    std::cout << "Debug: Semester: '" << semester << "'" << std::endl;
-
-    getline(iss, temp, ';');
-    std::cout << "Debug: Attempting to convert Grade: '" << temp << "'" << std::endl;
-    grade = stof(temp);
+    grade = std::stof(temp);
 }
+
